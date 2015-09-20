9f09e0e714da203eba7aef963c2026fce147ef75
samaitra <saikat.maitra@gmail.com>
2015-09-20 16:35:56 +0530

    added user and group

--- cookbooks/customers/recipes/user.rb
+++ cookbooks/customers/recipes/user.rb
@@ -3,3 +3,12 @@
 # Recipe:: user
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+group node['customers']['group']
+
+user node['customers']['user'] do
+  group node['customers']['group']
+  system true
+  shell '/bin/bash'
+end
+
